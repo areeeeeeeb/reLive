666c673b085df4b2b478cc233b6cc2c262e954f9
Areeb <arasul@uwaterloo.ca>
2026-01-17 18:10:44 -0500

    feat: implement auth0

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import { getRedirectUri } from './hooks/useAuth';
+
 defineCustomElements(window);
+
 const container = document.getElementById('root');
 const root = createRoot(container!);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      useRefreshTokens={true}
+      useRefreshTokensFallback={false}
+      cacheLocation="localstorage"
+      authorizationParams={{
+        redirect_uri: getRedirectUri()
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
