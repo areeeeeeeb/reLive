dcb0541d1cca16c416c60f928d785df431a3181a
Areeb <arasul@uwaterloo.ca>
2026-01-17 14:08:30 -0500

    feat: setup tailwind CSS

--- frontend/src/pages/Tab1.tsx
+++ frontend/src/pages/Tab1.tsx
@@ -1,6 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
@@ -11,6 +10,7 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <div className='size-16'></div>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 1</IonTitle>
