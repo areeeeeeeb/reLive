1b6d47719e184e95213d95209d3e10acd5201284
Areeb <arasul@uwaterloo.ca>
2026-01-17 18:25:08 -0500

    refactor: move tabs into a separate component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { IonApp, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -6,17 +6,9 @@ import { App as CapApp } from '@capacitor/app';
 import { Browser } from '@capacitor/browser';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
-import Signup from './pages/Signup';;
+import Signup from './pages/Signup';
 import { Route, Redirect } from 'react-router-dom';
-import { home, search, person, cloudUpload } from 'ionicons/icons';
-import Home from './pages/Home';
-import Search from './pages/Search';
-import UserProfile from './pages/Profile';
-import Event from './pages/Event';
-import Watch from './pages/Watch';
-import Upload from './pages/Upload';
-import Venue from './pages/Venue';
-import Artist from './pages/Artist';
+import Tabs from './components/tabs';
 
 // tailwind css
 import './theme/tailwind.css';
@@ -73,48 +65,7 @@ const App: React.FC = () => {
       <IonReactRouter>
         {isAuthenticated ? (
           // protected routes - user is logged in
-          <IonTabs>
-            <IonRouterOutlet>
-              {/* Tab roots */}
-              <Route exact path="/home" component={Home} />
-              <Route exact path="/search" component={Search} />
-              <Route exact path="/upload" component={Upload} />
-              <Route exact path="/profile" component={UserProfile} />
-
-              {/* Shared routes - accessible from any tab */}
-              <Route exact path="/event/:eventId" component={Event} />
-              <Route exact path="/event/:eventId/watch" component={Watch} />
-              <Route exact path="/event/:eventId/upload" component={Upload} />
-              <Route exact path="/venue/:venueId" component={Venue} />
-              <Route exact path="/artist/:artistId" component={Artist} />
-
-              <Route exact path="/tabs">
-                <Redirect to="/home" />
-              </Route>
-            </IonRouterOutlet>
-
-            <IonTabBar slot="bottom" className='pb-safe'>
-              <IonTabButton tab="home" href="/home">
-                <IonIcon icon={home} />
-                <IonLabel>Home</IonLabel>
-              </IonTabButton>
-
-              <IonTabButton tab="search" href="/search">
-                <IonIcon icon={search} />
-                <IonLabel>Search</IonLabel>
-              </IonTabButton>
-
-              <IonTabButton tab="upload" href="/upload">
-                <IonIcon icon={cloudUpload} />
-                <IonLabel>Upload</IonLabel>
-              </IonTabButton>
-
-              <IonTabButton tab="profile" href="/profile">
-                <IonIcon icon={person} />
-                <IonLabel>Profile</IonLabel>
-              </IonTabButton>
-            </IonTabBar>
-          </IonTabs>
+          <Tabs />
         ) : (
           // Public routes - user is not logged in
           <IonRouterOutlet>
