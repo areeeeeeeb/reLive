a69a6f0a7ad44adef9c6598d74a63a38a39b42f3
Areeb <arasul@uwaterloo.ca>
2026-01-17 12:45:00 -0500

    feat: remove template things

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.demo.pg.react',
-  appName: 'React Photo Gallery',
+  appId: 'com.hackathon.relive',
+  appName: 'reLive',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
