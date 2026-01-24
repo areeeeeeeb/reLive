12fa2738046b3f3a093f9aac6f22889e73ddae56
Aarush Ghosh <aarush.ghosh@gmail.com>
2026-01-24 13:27:10 -0500

    -used godotenv in config.go to parse env variables, - added package names for for handlers and models (my ide was being strict), - added a db health check endpoint in main.go

--- backend-go/config/config.go
+++ backend-go/config/config.go
@@ -1,28 +1,28 @@
 package config
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	Port		string
-	Environment	string
+	Port        string
+	Environment string
 	DatabaseURL string
 }
 
 func Load() *Config {
+	godotenv.Load()
 
 	return &Config{
-		Port:			getEnv("PORT", "8081"),
-		Environment:	getEnv("ENVIRONMENT", "development"),
-		DatabaseURL:	getEnv("DATABASE_URL", ""),
+		Port:        getEnv("PORT", "8081"),
+		Environment: getEnv("ENVIRONMENT", "development"),
+		DatabaseURL: getEnv("DATABASE_URL", ""),
 	}
 }
 
 func getEnv(key, defaultValue string) string {
-	fmt.Println(key)
-	fmt.Println(os.Getenv(key))
 	if value := os.Getenv(key); value != "" {
 		return value
 	}
