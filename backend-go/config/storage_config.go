5d89bfb64a8c12c03ae732ea382d6243973c8188
Aarush Ghosh <aarush.ghosh@gmail.com>
2026-01-25 01:12:18 -0500

    spaces and s3 config minimal setup + minimal upload handler interfaces written

--- backend-go/config/storage_config.go
+++ backend-go/config/storage_config.go
@@ -1,2 +1,25 @@
 package config
-// config for spaces (for now)
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func (c *Config) NewS3Client(ctx context.Context) (*s3.Client, error) {
+	if c.Spaces.AccessKey == "" || c.Spaces.SecretKey == "" {
+		return nil, fmt.Errorf("DO Spaces credentials not configured")
+	}
+
+	client := s3.New(s3.Options{
+		BaseEndpoint: aws.String(c.Spaces.Endpoint),
+		Region:       c.Spaces.Region,
+		Credentials:  credentials.NewStaticCredentialsProvider(c.Spaces.AccessKey, c.Spaces.SecretKey, ""),
+		UsePathStyle: true,
+	})
+
+	return client, nil
+}
