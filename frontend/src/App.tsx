58387b720eacf806e1ab1a20de7e3744e336f401
Areeb <arasul@uwaterloo.ca>
2026-01-31 16:46:56 -0500

    fix: stop ionic style overwriting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,8 +14,7 @@ import { ThemeProvider } from './contexts/ThemeContext';
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
+// import '@ionic/react/css/normalize.css';  // commented out to prevent global button/input/form resets
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
 
