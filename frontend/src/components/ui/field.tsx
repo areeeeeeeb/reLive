c144871942bd89f01ad2550e7bb9ddb616595247
Areeb <arasul@uwaterloo.ca>
2026-02-07 16:15:37 -0500

    ui: decrease field gap

--- frontend/src/components/ui/field.tsx
+++ frontend/src/components/ui/field.tsx
@@ -53,7 +53,7 @@ function FieldGroup({ className, ...props }: React.ComponentProps<"div">) {
 }
 
 const fieldVariants = cva(
-  "group/field flex w-full gap-3 data-[invalid=true]:text-destructive",
+  "group/field flex w-full gap-1 data-[invalid=true]:text-destructive",
   {
     variants: {
       orientation: {
@@ -97,7 +97,7 @@ function FieldContent({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="field-content"
       className={cn(
-        "group/field-content flex flex-1 flex-col gap-1.5 leading-snug",
+        "group/field-content flex flex-1 flex-col gap-2 leading-snug",
         className
       )}
       {...props}
